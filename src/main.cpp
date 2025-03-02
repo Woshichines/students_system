bcb4b62255da0f1535d42351337261285fcfe1a3
Zqh <zqh_201201172022@163.com>
2025-03-02 22:31:23 +0800

    改进指针管理，使用智能指针替代void指针，并扩展Teachers类功能

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,21 @@
 #include "../My_include/Schools"
 #include "../My_include/Teachers"
 #include "../My_include/Classrooms"
+using namespace std;
 
 int main()
 {
-    Students St("zqh");
-    St.减分();
-    St.输出分数();
+    shared_ptr<Students> St(new Students("zqh"));
+    shared_ptr<Teachers> Te(new Teachers("主任"));
+
+    St->减分();
+    cout << St->输出分数() << endl;
+
+    Te->加分(St, 3);
+    cout << St->输出分数() << endl;
+
+    
+
+    system("pause");
+    return 0;
 }
