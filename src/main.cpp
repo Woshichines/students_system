edb28bfd4c4c13c4bc4f36c9654cb4b373eb5d89
Zqh <zqh_201201172022@163.com>
2025-03-06 23:21:17 +0800

    未完成...

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,14 @@ using namespace std;
 
 int main()
 {   
-    setlocale(LC_ALL, "zh_CN.UTF-8");
+    setlocale(LC_ALL, "zh_CN.UTF-8"); //使用UTF-8
 
+    //智能指针 创建三个类
     shared_ptr<Classrooms> Cl(new Classrooms());
     shared_ptr<Students> St(new Students("zqh", NULL, Cl));
     shared_ptr<Teachers> Te(new Teachers("主任"));
+
+    // Te->添加班级c(Cl);
     
 
     St->减分();
